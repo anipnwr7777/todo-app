a6228acd3f84fae754614e76f3f3b205af36123d
Anirudh <anirudhpanwar7777@gmail.com>
2022-01-15 18:05:57 +0530

    added javascript

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 const input = document.getElementById('in');
 const button = document.getElementById('btn');
 const list = document.getElementById('list');
+let tempObj;
 
 button.addEventListener('click', (event)=>{
-    // console.log(input);
-    console.log(input.value);
-
     const newTask = document.createElement('li');
     const innerText = document.createTextNode(input.value);
 
@@ -15,7 +13,26 @@ button.addEventListener('click', (event)=>{
     input.value = "";
 })
 
-list.addEventListener('mouseover', (event)=>{
-    // alert("hovered");
-    
+list.addEventListener('mousemove', (event)=>{
+    if(event.target.tagName == 'LI' && !event.target.classList.contains('finished')){
+        event.target.classList.add('getting-finished');
+    }
+
+    if(event.target.tagName == 'LI' && event.target.classList.contains('finished')){
+        event.target.classList.add('revive-again');
+    }
+})
+
+list.addEventListener('mouseout', (event)=>{
+    if(event.target.tagName == 'LI'){
+        event.target.classList.remove('getting-finished', 'revive-again');
+    }
+})
+
+list.addEventListener('click', (event)=>{
+    if(event.target.tagName == 'LI' && event.target.classList.contains('finished')){
+        event.target.classList.remove('finished');
+    } else {
+        event.target.classList.add('finished');
+    }
 })
